aa81d2d19ac77bbaa2771f3c5d33bc63da5820da
Franciscoamaral0 <franciscogurgel@me.com>
2021-09-29 19:52:49 +0100

    add card/grid on dashboard

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from '@material-ui/core'
+import { Button, Container, Typography, Grid, CardMedia, CardContent, Card, CardActions } from '@material-ui/core'
 import TemplateDefault from '../../src/templates/Default'
 import { makeStyles } from '@material-ui/core'
 
@@ -6,7 +6,16 @@ import { makeStyles } from '@material-ui/core'
 const useStyles = makeStyles((theme) =>({
   container:{
     padding: theme.spacing(8,0,6)
-  }
+  },
+  sizeImage:{
+    paddingTop:'56%'
+  },
+  buttonAdd: {
+    margin: '30px auto',
+    display: 'block',
+
+  },
+
 }))
 
 const Dashboard = () => {
@@ -15,12 +24,152 @@ const Dashboard = () => {
   return (
     <TemplateDefault>
       <Container maxWidth='sm'>
-        <Typography component='h1' variant='h2' className={classes.container}>
+        <Typography component='h1' variant='h2' align='center' className={classes.container}>
           Meus Anuncios
         </Typography>
+          <Button variant='contained' color='primary' className={classes.buttonAdd}>
+            Publicar Novo Anúncio 
+          </Button>
+      </Container>
+
+      <Container maxWidth='md' >
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia
+                image={'https://source.unsplash.com/random'}
+                title='titulo imagem'
+                className={classes.sizeImage}
+              />
+              <CardContent>
+                <Typography variant='h5' component='h2'>
+                  Produto x
+                </Typography>
+                <Typography>
+                  €50,99
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size='small' color='primary'>Editar</Button>
+                <Button size='small' color='primary'>Editar</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </TemplateDefault>
   )
+
 }
 
 export default Dashboard
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,38 @@
 import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu'
-
+import { makeStyles } from '@material-ui/core';
 
 const Header = () =>  {
+
+  const useStyles = makeStyles((theme) => ({
+    root:{
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title:{
+      flexGrow: 1,
+    }
+  }))
+
+
+
+  const classes = useStyles()
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <div className={classes.root}> 
+      <AppBar position='static'>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
+            <MenuIcon/>
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Typography variant='h6' className={classes.title}>
+            E-Hub
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
 }
 
